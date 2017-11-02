e03118fd86a5731e87a497e3da0c67c1c70914a6
Dhruv Marwha <marwha.dhruv@gmail.com>
2017-11-02 12:10:29 +0530

    Update CNN Code.py
    New Code also submitted on SO.

--- CNN Code.py
+++ CNN Code.py
@@ -1,77 +1,68 @@
-import os
-import sys
-import tarfile
-import numpy as np
-import scipy.io as sio
-from six.moves import cPickle as pickle
-import tensorflow as tf
-from PIL import Image
-import datetime
-import matplotlib.pyplot as plt
-def OneHot(label,n_classes):
-    label=np.array(label).reshape(-1)
-    label=np.eye(n_classes)[label]
+filename='extra.pickle'
 
-    return label
+with open(filename,'rb') as f:
+    other=pickle.load(f)
+    #print(other)
 
-data_file = 'extra.pickle'
 
-print('Tring to load pickle from %s' % data_file)
-with open(data_file, 'rb') as file:
-    svhn_datasets = pickle.load(file)
-    train_dataset = svhn_datasets['train_dataset']
-    test_dataset = svhn_datasets['test_dataset']
-    del svhn_datasets # free up memory
-print('pickle loaded successfully!')
+    train_data=other['train_dataset']
+    test_data=other['test_dataset']
+    #print(test_data)
 
-train_data = train_dataset['X']
-train_labels = train_dataset['y']
+    del other
 
-test_data = test_dataset['X']
-test_labels = test_dataset['y']
+train_dataset=train_data['X']
+test_dataset=test_data['X']
+#print(train_dataset)
+#train_data = np.transpose(train_data, (3, 0, 1, 2))
+train_labels=train_data['y']
+test_labels=test_data['y']
 
-del train_dataset, test_dataset
+print(len(test_dataset))
+print(len(test_labels))
+print(test_dataset.shape)
+#print(train_dataset.length())
 
-print('Test data:', test_data.shape,', Test labels:', test_labels.shape)
+classes=10
 
-from sklearn.cross_validation import train_test_split
+batch_size=32
 
-print ('Train data:', train_data.shape,', Train labels:', train_labels.shape)
-#print ('Validation data:',,', Validation labels:', valid_labels.shape)
+num_steps = 200000
 
-graph = tf.Graph()
+graph=tf.Graph()
 
+#Placeholder for the data
 with graph.as_default():
-    # placeholders for input data batch_size x 32 x 32 x 3 and labels batch_size x 10
-    data_placeholder = tf.placeholder(tf.float32, shape=[None, 32, 32, 3])
-    label_placeholder = tf.placeholder(tf.float32, shape=[None, 10])
 
-    # defining decaying learning rate
-    global_step = tf.Variable(0)
-    decay_rate = tf.train.exponential_decay(1e-4, global_step=global_step, decay_steps=10000, decay_rate=0.97)
+    data_placeholder = tf.placeholder(tf.float32, shape=(batch_size,32,32,3))
+    label_placeolder = tf.placeholder(tf.int64, shape=(batch_size, classes))
 
-    layer1_weights = tf.Variable(tf.truncated_normal([3, 3, 3, 64],stddev=0.1))
-    layer1_biases = tf.Variable(tf.constant(0.1, shape=[64]))
+    tf_test_dataset = tf.placeholder(tf.float32, shape=(batch_size,32,32,3))
 
-    layer2_weights = tf.Variable(tf.truncated_normal([3, 3, 64,32],stddev=0.1))
-    layer2_biases = tf.Variable(tf.constant(0.1,shape=[32]))
+    tf_label_dataset = tf.placeholder(tf.float32, shape=(batch_size, classes))
+    #Variable for Weights and Biases
+    #1024 is for the number of nodes in each hidden layer
 
-    layer3_weights = tf.Variable(tf.truncated_normal([2, 2, 32, 4096],stddev=0.1))
-    layer3_biases = tf.Variable(tf.constant(0.1,shape=[4096]))
+    layer1_weights=tf.Variable(tf.truncated_normal([3,3,3,16]))
+    layer1_biases=tf.Variable(tf.zeros([16]))
 
-    layer4_weights = tf.Variable(tf.truncated_normal([4096,10],stddev=0.1))
-    layer4_biases = tf.Variable(tf.constant(0.1,shape=[10]))
+    layer2_weights=tf.Variable(tf.truncated_normal([3,3,16,32]))
+    layer2_biases=tf.Variable(tf.zeros([32]))
 
-    layer5_weights = tf.Variable(tf.truncated_normal([1000, 10], stddev=0.1))
-    layer5_biases = tf.Variable(tf.constant(0.1, shape=[10]))
+    layer3_weights=tf.Variable(tf.truncated_normal([2,2,32,64]))
+    layer3_biases=tf.Variable(tf.zeros([64]))
 
+    layer4_weights=tf.Variable(tf.truncated_normal([1024,10]))
+    layer4_biases=tf.Variable(tf.zeros([10]))
 
+    layer5_weights=tf.Variable(tf.truncated_normal([10,classes]))
+    layer5_biases=tf.Variable(tf.zeros([classes]))
 
-    def layer_multiplication(data_input_given):
+    #The last layer would be a fsoftmax layer with decay rate and l2 Regualrization
+
+    def layer_multiplication(data_input_given,dropping=False):
 
         #Convolutional Layer 1
-
-        data_input_given=np.reshape(data_input_given,[-1,32,32,3])
 
         CNN1=tf.nn.relu(tf.nn.conv2d(data_input_given,layer1_weights,strides=[1,1,1,1],padding='SAME')+layer1_biases)
 
@@ -92,6 +83,8 @@ with graph.as_default():
         print('pool2 Done')
         #Third Convolutional Layer
 
+        print(Pool2.shape)
+
         CNN3 = tf.nn.relu(tf.nn.conv2d(Pool2, layer3_weights, strides=[1, 1, 1, 1], padding='SAME')) + layer3_biases
         print('CNN3 Done')
         #Third Pooling Layer
@@ -100,54 +93,103 @@ with graph.as_default():
         print('Pool3 DOne')
         #Fully Connected Layer
 
-        FullyCon=tf.reshape(Pool3,[-1,4096])
+        #print(Pool3.shape)
 
-        FullyCon=tf.nn.relu(tf.matmul(FullyCon,layer4_weights)+layer4_biases)
+        shape = Pool3.get_shape().as_list()
 
-        print('Fullyconnected Done')
-        dropout = tf.nn.dropout(FullyCon, 0.4)
+        # print(shape)
 
-        dropout=tf.reshape(dropout,[-1,1000])
+        reshape = tf.reshape(Pool3, [shape[0], shape[1] * shape[2] * shape[3]])
 
-        dropout=tf.matmul(dropout,layer5_weights)+layer5_biases
+        #print(reshape.shape)
 
-        print(dropout.shape)
+        FullyCon = tf.nn.relu(tf.matmul(reshape, layer4_weights) + layer4_biases)
 
+        #print(FullyCon.shape)
 
-        return dropout
+        if dropping==False:
+            print('Training')
+            dropout = tf.nn.dropout(FullyCon, 0.6)
+            z=tf.matmul(dropout,layer5_weights)+layer5_biases
+            return z
 
-
-    train_input = layer_multiplication(train_data)
-    print(train_input.shape)
-
-    loss = (tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=label_placeholder, logits=train_input))
-            + 0.01 * tf.nn.l2_loss(layer1_weights)
-            + 0.01 * tf.nn.l2_loss(layer2_weights)
-            + 0.01 * tf.nn.l2_loss(layer3_weights)
-            + 0.01 * tf.nn.l2_loss(layer4_weights)
-            )
-
-    optimizer = tf.train.GradientDescentOptimizer(name='Stochastic', learning_rate=decay_rate).minimize(loss,global_step=global_step)
-
-    #print(train_input.shape)
+        else:
+            print('Testing')
+            z = tf.matmul(FullyCon, layer5_weights) + layer5_biases
+            return z
 
 
-    batch_size = 64
+    gloabl_step = tf.Variable(0, trainable=False)
 
-    num_steps=100000
+    decay_rate=tf.train.exponential_decay(1e-6,gloabl_step,4000,0.96,staircase=False,)
 
-    with tf.Session(graph=graph) as session:
+    train_input=layer_multiplication(data_placeholder,False)
+
+    test_prediction = tf.nn.softmax(layer_multiplication(tf_test_dataset,True))
+
+    #correct_prediction = tf.equal(tf.argmax(test_prediction, 1), tf.argmax(label_placeolder, 1))
+
+    #accu = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    loss=(tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=label_placeolder,logits=train_input))
+                                   + 0.01 * tf.nn.l2_loss(layer1_weights)
+                                   + 0.01 * tf.nn.l2_loss(layer2_weights)
+                                   + 0.01 * tf.nn.l2_loss(layer3_weights)
+                                   + 0.01 * tf.nn.l2_loss(layer4_weights)
+                                   + 0.01 * tf.nn.l2_loss(layer5_weights)
+                                   )
+
+    optimizer = tf.train.GradientDescentOptimizer(name='Stochastic', learning_rate=decay_rate).minimize(loss,global_step=gloabl_step)
+
+
+    def accuracy(predictions, labels):
+        print(predictions.shape[0])
+        return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
+                / predictions.shape[0])
+
+    config=tf.ConfigProto()
+    config.gpu_options.allocator_type ='BFC'
+
+    saver = tf.train.Saver()
+
+    test_accuracy=[]
+
+    with tf.Session(config=config) as session:
         tf.global_variables_initializer().run()
         print('Initialized')
-        for i in range(num_steps):
-            print("in loop")
-            offset = (i * batch_size) % (train_labels.shape[0] - batch_size)
-            batch_data = train_data[offset:(offset + batch_size), :, :]
+        tf.train.write_graph(session.graph_def, '.', './SVHN.pbtxt')
+
+        for step in range(num_steps):
+            offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
+            batch_data = train_dataset[offset:(offset + batch_size), :, :]
             batch_labels = train_labels[offset:(offset + batch_size), :]
 
-            print("training")
-            feed_dict = {data_placeholder: batch_data, label_placeholder: batch_labels}
+            batch_test_data =  test_dataset[offset:(offset + batch_size), :, :]
+            batch_test_labels = test_labels[offset:(offset + batch_size),:]
+            #print(batch_data)
+            #print(batch_test.shape)
+
+            feed_dict = {data_placeholder:batch_data, label_placeolder:batch_labels}
             _, l, predictions = session.run(
                 [optimizer, loss, train_input], feed_dict=feed_dict)
-            print('Finished')
+            if (step % 500 == 0):
+                #print(session.run(decay_rate))
+                print('Minibatch loss at step %d: %f' % (step, l))
+                print('Minibatch accuracy: %.1f%%' % accuracy(predictions, batch_labels))
+                if(batch_test_data.shape!=(32,32,32,3)):
+                    print('Skip')
+                else:
+                    correct_prediction = tf.equal(tf.argmax(test_prediction, 1), tf.argmax(tf_label_dataset, 1))
+                    accuracy_for_test = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+                    print("Test Accuracy")
 
+                    test_accuracy.append(accuracy_for_test.eval(feed_dict={tf_test_dataset:batch_test_data,
+                                                tf_label_dataset:batch_test_labels}))
+
+                    print(accuracy_for_test.eval(feed_dict={tf_test_dataset:batch_test_data,
+                                                tf_label_dataset:batch_test_labels}))
+
+        print(np.mean(test_accuracy))
+
+
+        saver.save(sess=session, save_path='./SVHN.ckpt')
